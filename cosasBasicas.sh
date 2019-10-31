c858d4c4cb857e787d9244c41870528f2e0bc271
Oscar Hugo Cardenas Lopez <oscardev@cacyberlo.com.mx>
2019-10-31 10:39:01 -0600

    refactorizando

--- cosasBasicas.sh
+++
@@ -1,3 +0,0 @@
-sudo apt install gnome-tweak-tool git git-core unrar wine64 python-pip
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-sudo pip install wakatime
