0cee875801030538e64a39604ef4866acca7ab09
Oscar Hugo Cardenas Lopez <oscardev@cacyberlo.com.mx>
2022-01-18 11:38:26 -0600

    Update cleanDocker.sh

--- cleanDocker.sh
+++ cleanDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker volume rm $(docker volume ls -q --filter dangling=true)
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
+docker volume rm $(docker volume ls -q)
 docker rmi $(docker images -q) --force
